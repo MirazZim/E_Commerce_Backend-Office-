53a76d915c0b8371a2a9434ea1d3a0f99e606ec7
MirazZim <miraz.zim.38@gmail.com>
2025-08-19 12:02:20 +0600

    This is how The Office wants me to declare functions

--- src/Server.js
+++ src/Server.js
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser");
 
 const { pool } = require("./DB/pool");
 const userRoutes = require("./routes/userRoutes");
+const customerRoutes = require("./routes/customerRoutes");
 
 app.use(bodyParser.json());
 app.use(morgan("combined"));
@@ -27,7 +28,7 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 
 // Routes
-app.use("/api", userRoutes);
+app.use("/users", userRoutes);
 // Database connection check
 pool
   .getConnection()
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,71 @@
 const express = require("express");
-const {
-  registerUser,
-  loginUser,
-  getUserDetails,
-} = require("../controllers/userController");
 const jwtAuth = require("../middleware/jwtAuth");
+const { validateUserInput } = require("../middleware/register/register-data-validator");
+const { registerUser } = require("../main/users/user-register");
+const { validateLoginInput } = require("../middleware/login/login-data-validator");
+const { loginUser } = require("../main/users/user-login");
+const { getUserDetails } = require("../main/users/user-details");
 
 const router = express.Router();
 
-router.post("/users/register", registerUser);
-router.post("/users/login", loginUser);
-router.get("/users/:id", jwtAuth, getUserDetails);
+router.post("/register", validateUserInput, async (req, res) => {
+    registerUser(req.body)
+        .then((data) => {
+            return res.status(200).send({
+                status: data.status,
+                message: data.message,
+            });
+        })
+        .catch((error) => {
+            return res.status(400).send({
+                status: error.status,
+                message: error.message,
+            });
+        });
+});
+
+
+router.post("/login", validateLoginInput, async (req, res) => {
+    loginUser(req.body)
+        .then((data) => {
+            return res.status(200).send({
+                status: data.status,
+                message: data.message,
+                token: data.token,
+                user: data.user,
+            });
+        })
+        .catch((error) => {
+            return res.status(400).send({
+                status: error.status,
+                message: error.message,
+            });
+        });
+})
+
+router.get("/:id", jwtAuth, async (req, res) => {
+    const requestData = {
+        userId: req.params.id,
+        requestingUser: req.user
+    };
+
+    getUserDetails(requestData)
+        .then((data) => {
+            return res.status(200).send({
+                status: data.status,
+                message: data.message,
+                user: data.user,
+            });
+        })
+        .catch((error) => {
+            return res.status(400).send({
+                status: error.status,
+                message: error.message,
+            });
+        });
+});
+
+
+
 
 module.exports = router;
