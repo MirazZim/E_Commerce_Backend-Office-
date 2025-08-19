eabe3c96da44785bd7292dc47f8cc7aef341431c
MirazZim <miraz.zim.38@gmail.com>
2025-08-19 16:32:25 +0600

    In Customer Route Added Get Customer details Function and used Admin role for access

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwtAuth = require("../middleware/jwtAuth");
 const { registerCustomer } = require("../main/customers/add-customer");
 const { getAllCustomers } = require("../main/customers/get-all-customers");
+const { getCustomerDetails } = require("../main/customers/get-customer-details");
 
 const customerRouter = express.Router();
 
@@ -33,6 +34,29 @@ customerRouter.get("/", jwtAuth, async (req, res) => {
                 status: data.status,
                 message: data.message,
                 customers: data.customers,
+            });
+        })
+        .catch((error) => {
+            return res.status(400).send({
+                status: error.status,
+                message: error.message,
+            });
+        });
+});
+
+
+customerRouter.get("/get-customer-details", jwtAuth, async (req, res) => {
+    const requestData = {
+        customerId: req.query.id,
+        user: req.user
+    };
+
+    getCustomerDetails(requestData)
+        .then((data) => {
+            return res.status(200).send({
+                status: data.status,
+                message: data.message,
+                customer: data.customer,
             });
         })
         .catch((error) => {
